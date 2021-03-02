375e0c22215bf030b733049aaa6376c0a51ae02a
TonyMartinsson <tony.martinsson@hotmail.com>
2021-03-02 13:32:51 +0100

    Wrapped errorboundary around layout

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Layout from './Layout';
 import { BrowserRouter as Router } from 'react-router-dom';
+import ErrorBoundary from '../ErrorBoundary';
 
 function App() {
   return (
-    
-      <Router>
-        <Layout />
+    <Router>
+        <ErrorBoundary>
+          <Layout />
+      </ErrorBoundary>
       </Router>
   );
 }
